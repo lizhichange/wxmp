0821a2e5ae9d25526dcec495a6fb605e7d2e1f50
lizhichange@163.com <Lizhi1234>
2019-12-19 17:48:48 +0800

    1

--- src/main/java/com/wxmp/wxmp/NotifyController.java
+++ src/main/java/com/wxmp/wxmp/NotifyController.java
@@ -3,6 +3,7 @@ package com.wxmp.wxmp;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +18,7 @@ public class NotifyController {
 
 
     @GetMapping("/notify")
+    @PostMapping("/notify")
     public String xxx(HttpServletRequest request) {
           log.info("notify:{}",request);
          return "SUCCESS";
